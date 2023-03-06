c96c930c5f017f3ae7c7697363f8a9e1e7eb66b1
lyc <1160282114@qq.com>
2023-03-06 13:41:57 +1100

    11111

--- src/Diamond2.java
+++ src/Diamond2.java
@@ -1,6 +1,6 @@
 public class Diamond2 {
     public static void main(String[] args) {
-        print(7); // 输出7行的菱形
+        print(5); // 输出7行的菱形
     }
 
     public static void print(int size) {
@@ -13,7 +13,7 @@ public class Diamond2 {
             }
             for (int j = 0; j < 2 * i + 1; j++) {
 
-                System.out.print("*"); // 输出菱形上半部边缘
+                System.out.print("#"); // 输出菱形上半部边缘
 
             }
             System.out.println(); // 换行
@@ -24,7 +24,7 @@ public class Diamond2 {
             }
             for (int j = 0; j < 2 * size - 1 - 2 * i; j++) {
 
-                System.out.print("*"); // 输出菱形下半部边缘
+                System.out.print("#"); // 输出菱形下半部边缘
 
             }
             System.out.println(); // 换行
