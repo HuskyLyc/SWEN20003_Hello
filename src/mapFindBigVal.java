08c3f81b012bd262b1a44f6941b7d6464d235082
lyc <1160282114@qq.com>
2023-03-06 13:32:40 +1100

    11111

--- src/mapFindBigVal.java
+++ src/mapFindBigVal.java
@@ -3,7 +3,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-public class mapFindBigVal {
+public class mapFindBigVal{
     public static void main(String[] args) {
             Map<String, Integer> map = new HashMap();
             map.put("1", 8);
