d5cf012e02353d4490f0f8d9f1d05d47723f36e8
lyc <1160282114@qq.com>
2023-03-21 09:15:44 +1100

    11111

---
+++ src/week4/show.java
@@ -0,0 +1,13 @@
+package week4;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class show {
+    private String name;
+    SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+    Date beginTime = df.format();
+    Date overTime = df.format();
+    Date duration = df.
+}
